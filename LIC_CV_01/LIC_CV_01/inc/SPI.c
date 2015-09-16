56789731b964bbf6fc3e708928d4619d0e455063
CeitecBrno <lukasanov@gmail.com>
2015-09-16 13:26:16 +0200

    Totalni uprava
    Ale melo byto fungovat...

--- LIC_CV_01/LIC_CV_01/inc/SPI.c
+++ LIC_CV_01/LIC_CV_01/inc/SPI.c
@@ -15,7 +15,7 @@
 void spi_init()
 {
 	SPI_DDR = (1<<SPI_MOSI) | (1<<SPI_SCK) | (1<<SPI_SS);
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);//prescaler 16
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<SPR1);//prescaler 128
 }
 
 
